abc74f1149a45fe9d35278676f0cde8d074e9378
uglimario <bs.uglimario@gmail.com>
2023-12-27 14:47:22 +0100

    mértékegység átváltó div-ek, updates recept-index

--- Kalkulatorok/Script/kalkulator.js
+++ Kalkulatorok/Script/kalkulator.js
@@ -1,3 +1,13 @@
-function fontKilogramkonverter(valNUM) {
+function fontKilogrammkonverter(valNUM) {
     document.getElementById("outputkilogramm").innerHTML = valNUM * 0.4536;
+}
+function Kilogrammfontkonverter(valNUM) {
+    document.getElementById("outputfont").innerHTML = valNUM / 0.4536;
+}
+
+function Inchcentikonverter(valNUM) {
+    document.getElementById("outputcenti").innerHTML = valNUM * 2.54;
+}
+function centiInchkonverter(valNUM) {
+    document.getElementById("outputinch").innerHTML = valNUM / 2.54;
 }
