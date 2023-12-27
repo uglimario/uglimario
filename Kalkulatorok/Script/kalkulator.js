11cfd4ecf9d750fa9f1dbfef0eb06d7322eb6b8a
uglimario <bs.uglimario@gmail.com>
2023-12-27 15:34:54 +0100

    tizedesjegyek csokkentese 3ra

--- Kalkulatorok/Script/kalkulator.js
+++ Kalkulatorok/Script/kalkulator.js
@@ -1,13 +1,19 @@
-function fontKilogrammkonverter(valNUM) {
-    document.getElementById("outputkilogramm").innerHTML = valNUM * 0.4536;
-}
-function Kilogrammfontkonverter(valNUM) {
-    document.getElementById("outputfont").innerHTML = valNUM / 0.4536;
+function fontKilogrammkonverter(valNUM, decimalPlaces) {
+    var kilogrammErtek = (valNUM * 0.4536).toFixed(3);
+    document.getElementById("outputkilogramm").innerHTML = kilogrammErtek;
 }
 
-function Inchcentikonverter(valNUM) {
-    document.getElementById("outputcenti").innerHTML = valNUM * 2.54;
+function Kilogrammfontkonverter(valNUM, decimalPlaces) {
+    var fontErtek = (valNUM / 0.4536).toFixed(3);
+    document.getElementById("outputfont").innerHTML = fontErtek;
 }
-function centiInchkonverter(valNUM) {
-    document.getElementById("outputinch").innerHTML = valNUM / 2.54;
+
+function Inchcentikonverter(valNUM, decimalPlaces) {
+    var centiErtek = (valNUM * 2.54).toFixed(3);
+    document.getElementById("outputcenti").innerHTML = centiErtek;
+}
+
+function centiInchkonverter(valNUM, decimalPlaces) {
+    var inchErtek = (valNUM / 2.54).toFixed(3);
+    document.getElementById("outputinch").innerHTML = inchErtek;
 }
